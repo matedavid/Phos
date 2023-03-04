a89fa190f33ec9a8b0a17fd31abd37b19a8ad943
matedavid <david.mate.lopez@gmail.com>
2023-03-04 13:08:00 +0100

    validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <stdexcept>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -31,7 +32,7 @@ class HelloTriangleApplication {
     }
 
     void init_vulkan() {
-        // Create instance
+        // CREATE INSTANCE
         VkApplicationInfo app_info{};
         app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         app_info.pApplicationName = "Hello Triangle";
@@ -40,8 +41,24 @@ class HelloTriangleApplication {
         app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         app_info.apiVersion = VK_API_VERSION_1_0;
 
+        // Extensions
         uint32_t glfw_extension_count = 0;
         const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+        // Validation layers
+        const std::vector<const char*> validation_layers = {
+            "VK_LAYER_KHRONOS_validation"
+        };
+
+#ifdef NDEBUG
+        const bool enable_validation_layers = false;
+#else
+        const bool enable_validation_layers = true;
+#endif
+
+        if (enable_validation_layers && !check_validation_layer_support(validation_layers)) {
+            throw std::runtime_error("Validation layers requested but not available");
+        }
 
         VkInstanceCreateInfo create_info{};
         create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -50,10 +67,42 @@ class HelloTriangleApplication {
         create_info.ppEnabledExtensionNames = glfw_extensions;
         create_info.enabledLayerCount = 0;
 
-        VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
+        if (enable_validation_layers) {
+            create_info.enabledLayerCount = (uint32_t)validation_layers.size();
+            create_info.ppEnabledLayerNames = validation_layers.data();
+        } else {
+            create_info.enabledLayerCount = 0;
+        }
+
+        const VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
         if (result != VK_SUCCESS) {
             throw std::runtime_error("Failed to create instance");
         }
+    }
+
+    bool check_validation_layer_support(const std::vector<const char*>& validation_layers) {
+        uint32_t layer_count;
+        vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+        std::vector<VkLayerProperties> layers(layer_count);
+        vkEnumerateInstanceLayerProperties(&layer_count, layers.data());
+
+        for (const std::string layer_name : validation_layers) {
+            bool layer_found = false;
+
+            for (const auto& layer_properties : layers) {
+                if (layer_name == std::string(layer_properties.layerName)) {
+                    layer_found = true;
+                    break;
+                }
+            }
+
+            if (!layer_found) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     void main_loop() {
