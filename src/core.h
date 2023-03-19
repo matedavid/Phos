1b1957b267ee6e4a4f77d03ca92fe2cb1be113ba
matedavid <david.mate.lopez@gmail.com>
2023-03-20 00:21:57 +0100

    core and changes to cmake

---
+++ src/core.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <cassert>
+
+#define CORE_INFO(...) spdlog::info(__VA_ARGS__)
+#define CORE_ERROR(...) spdlog::error(__VA_ARGS__)
+
+#define CORE_ASSERT(condition, ...) \
+    if (!condition) {               \
+        CORE_ERROR(__VA_ARGS__);    \
+        assert(false);              \
+    }
+
+#define CORE_FAIL(...)       \
+    CORE_ERROR(__VA_ARGS__); \
+    assert(false)
