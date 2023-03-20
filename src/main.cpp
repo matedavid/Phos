460a83dec7e4870342314a638af8010bab03594e
matedavid <david.mate.lopez@gmail.com>
2023-03-20 23:49:32 +0100

    restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream>
 #include <string>
 #include <vector>
-
-#include "renderer/vulkan_instance.h"
 
 constexpr uint32_t WIDTH = 1280;
 constexpr uint32_t HEIGHT = 960;
@@ -18,14 +15,12 @@ int main() {
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     uint32_t number_extensions;
-    const auto glfw_extensions = glfwGetRequiredInstanceExtensions(&number_extensions);
+    const auto& glfw_extensions = glfwGetRequiredInstanceExtensions(&number_extensions);
 
     std::vector<const char*> extensions{};
     for (uint32_t i = 0; i < number_extensions; ++i) {
         extensions.push_back(glfw_extensions[i]);
     }
-
-    const auto instance = std::make_unique<VulkanInstance>(extensions);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
