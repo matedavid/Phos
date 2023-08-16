98a7070b5fb7ec9046caa22b70800899fee8078b
matedavid <david.mate.lopez@gmail.com>
2023-08-16 20:40:50 +0200

    Show rendered image to screen

--- editor/main.cpp
+++ editor/main.cpp
@@ -7,11 +7,27 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_vulkan.h"
 
+#include "asset/asset_manager.h"
+
+#include "scene/scene_renderer.h"
+#include "scene/scene.h"
+#include "scene/entity.h"
+
+#include "managers/shader_manager.h"
+
+#include "renderer/camera.h"
+#include "renderer/mesh.h"
+#include "renderer/deferred_renderer.h"
 #include "renderer/backend/renderer.h"
+#include "renderer/backend/material.h"
+#include "renderer/backend/shader.h"
+#include "renderer/backend/vulkan/vulkan_descriptors.h"
 #include "renderer/backend/vulkan/vulkan_context.h"
 #include "renderer/backend/vulkan/vulkan_instance.h"
 #include "renderer/backend/vulkan/vulkan_device.h"
 #include "renderer/backend/vulkan/vulkan_command_buffer.h"
+#include "renderer/backend/vulkan/vulkan_texture.h"
+#include "renderer/backend/vulkan/vulkan_image.h"
 
 constexpr uint32_t WIDTH = 1280;
 constexpr uint32_t HEIGHT = 960;
@@ -19,10 +35,36 @@ constexpr uint32_t HEIGHT = 960;
 class EditorLayer : public Phos::Layer {
   public:
     EditorLayer() {
+        m_asset_manager =
+            std::make_shared<Phos::AssetManager>(std::make_shared<Phos::AssetPack>("../assets/asset_pack.psap"));
+        m_scene = std::make_shared<Phos::Scene>("Example");
+        m_renderer = std::make_shared<Phos::DeferredRenderer>(m_scene);
+
+        create_scene();
+
+        const auto aspect_ratio = WIDTH / HEIGHT;
+        const auto m_camera =
+            std::make_shared<Phos::PerspectiveCamera>(glm::radians(90.0f), aspect_ratio, 0.001f, 40.0f);
+        m_camera->set_position({0.0f, 3.0f, 7.0f});
+        m_camera->rotate(glm::vec2(0.0f, glm::radians(30.0f)));
+
+        m_scene->set_camera(m_camera);
+
         // Descriptor pool
         VkDescriptorPoolSize pool_sizes[] = {
-            {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1},
+            {VK_DESCRIPTOR_TYPE_SAMPLER, 100},
+            {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 100},
+            {VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 100},
+            {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 100},
+            {VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 100},
+            {VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 100},
+            {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 100},
+            {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 100},
+            {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 100},
+            {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 100},
+            {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 100},
         };
+
         VkDescriptorPoolCreateInfo pool_info = {};
         pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
         pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
@@ -61,7 +103,7 @@ class EditorLayer : public Phos::Layer {
         init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
         init_info.Allocator = nullptr;
         init_info.CheckVkResultFn = [](VkResult result) {
-            VK_CHECK(result);
+            VK_CHECK(result)
         };
         ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
 
@@ -73,6 +115,13 @@ class EditorLayer : public Phos::Layer {
 
         Phos::Renderer::wait_idle();
         ImGui_ImplVulkan_DestroyFontUploadObjects();
+
+        // Image Descriptors
+        const auto output_texture = std::dynamic_pointer_cast<Phos::VulkanTexture>(m_renderer->output_texture());
+        const auto image = std::dynamic_pointer_cast<Phos::VulkanImage>(output_texture->get_image());
+
+        m_set = ImGui_ImplVulkan_AddTexture(
+            output_texture->sampler(), image->view(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
     }
 
     ~EditorLayer() override {
@@ -94,8 +143,11 @@ class EditorLayer : public Phos::Layer {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        bool show = true;
-        ImGui::ShowDemoWindow(&show);
+        // bool show = true;
+        // ImGui::ShowDemoWindow(&show);
+
+        m_renderer->render();
+        ImGui::Image((ImTextureID)m_set, ImVec2(WIDTH, HEIGHT));
 
         // Rendering
         ImGui::Render();
@@ -106,14 +158,19 @@ class EditorLayer : public Phos::Layer {
             wd->ClearValue.color.float32[1] = 0.0f;
             wd->ClearValue.color.float32[2] = 0.0f;
             wd->ClearValue.color.float32[3] = 1.0f;
-            FrameRender(wd, draw_data);
-            FramePresent(wd);
+            render_frame(wd, draw_data);
+            present_frame(wd);
         }
     }
 
   private:
+    std::shared_ptr<Phos::ISceneRenderer> m_renderer;
+    std::shared_ptr<Phos::Scene> m_scene;
+    std::shared_ptr<Phos::AssetManager> m_asset_manager;
+
     ImGui_ImplVulkanH_Window* wd = new ImGui_ImplVulkanH_Window{};
     VkDescriptorPool m_descriptor_pool;
+    VkDescriptorSet m_set;
 
     void setup_vulkan_window() {
         Phos::Application::instance()->get_window()->create_surface(Phos::VulkanContext::instance->handle(),
@@ -150,7 +207,7 @@ class EditorLayer : public Phos::Layer {
                                                2);
     }
 
-    static void FrameRender(ImGui_ImplVulkanH_Window* wd, ImDrawData* draw_data) {
+    static void render_frame(ImGui_ImplVulkanH_Window* wd, ImDrawData* draw_data) {
         VkResult err;
 
         VkDevice device = Phos::VulkanContext::device->handle();
@@ -219,7 +276,7 @@ class EditorLayer : public Phos::Layer {
         }
     }
 
-    static void FramePresent(ImGui_ImplVulkanH_Window* wd) {
+    static void present_frame(ImGui_ImplVulkanH_Window* wd) {
         VkSemaphore render_complete_semaphore = wd->FrameSemaphores[wd->SemaphoreIndex].RenderCompleteSemaphore;
         VkPresentInfoKHR info = {};
         info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
@@ -235,6 +292,119 @@ class EditorLayer : public Phos::Layer {
         }
         VK_CHECK(err);
         wd->SemaphoreIndex = (wd->SemaphoreIndex + 1) % wd->ImageCount; // Now we can use the next set of semaphores
+    }
+
+    void create_scene() {
+        const auto sphere_mesh = m_asset_manager->load<Phos::Mesh>("../assets/models/sphere/sphere.psa");
+        const auto cube_mesh = m_asset_manager->load<Phos::Mesh>("../assets/models/cube/cube.psa");
+
+        // Floor
+        const auto floor_material = Phos::Material::create(
+            Phos::Renderer::shader_manager()->get_builtin_shader("PBR.Geometry.Deferred"), "Floor Material");
+
+        floor_material->set("uAlbedoMap", Phos::Texture::create("../assets/wood_mat/hardwood-brown-planks-albedo.png"));
+        floor_material->set("uMetallicMap",
+                            Phos::Texture::create("../assets/wood_mat/hardwood-brown-planks-metallic.png"));
+        floor_material->set("uRoughnessMap",
+                            Phos::Texture::create("../assets/wood_mat/hardwood-brown-planks-roughness.png"));
+        floor_material->set("uAOMap", Phos::Texture::create("../assets/wood_mat/hardwood-brown-planks-ao.png"));
+        floor_material->set("uNormalMap",
+                            Phos::Texture::create("../assets/wood_mat/hardwood-brown-planks-normal-ogl.png"));
+
+        PS_ASSERT(floor_material->bake(), "Failed to bake floor material")
+
+        const auto light_material = Phos::Material::create(
+            Phos::Renderer::shader_manager()->get_builtin_shader("PBR.Geometry.Deferred"), "Light Material");
+        PS_ASSERT(light_material->bake(), "Failed to bake light material")
+
+        auto floor_entity = m_scene->create_entity();
+        floor_entity.get_component<Phos::TransformComponent>().scale = glm::vec3(10.0f, 0.25f, 10.0f);
+        floor_entity.get_component<Phos::TransformComponent>().position = glm::vec3(0.0f, -0.25f, 0.0f);
+        floor_entity.add_component<Phos::MeshRendererComponent>({
+            .mesh = cube_mesh,
+            .material = floor_material,
+        });
+
+        auto wall_entity = m_scene->create_entity();
+        wall_entity.get_component<Phos::TransformComponent>().scale = glm::vec3(10.0f, 0.25f, 7.0f);
+        wall_entity.get_component<Phos::TransformComponent>().position = glm::vec3(0.0f, 6.5f, -10.0f);
+        wall_entity.get_component<Phos::TransformComponent>().rotation = glm::vec3(glm::radians(90.0f), 0.0f, 0.0f);
+        wall_entity.add_component<Phos::MeshRendererComponent>({
+            .mesh = cube_mesh,
+            .material = floor_material,
+        });
+
+        // Spheres
+        const uint32_t number = 4;
+        for (uint32_t metallic_idx = 0; metallic_idx < number; ++metallic_idx) {
+            for (uint32_t roughness_idx = 0; roughness_idx < number; ++roughness_idx) {
+                const auto sphere_material = Phos::Material::create(
+                    Phos::Renderer::shader_manager()->get_builtin_shader("PBR.Geometry.Deferred"), "Sphere Material");
+
+                const float metallic = (float)(metallic_idx + 1) / (float)number;
+                const float roughness = (float)(roughness_idx + 1) / (float)number;
+
+                sphere_material->set("uMaterialInfo.albedo", glm::vec3(0.8f, 0.05f, 0.05f));
+                sphere_material->set("uMaterialInfo.metallic", metallic);
+                sphere_material->set("uMaterialInfo.roughness", roughness);
+                sphere_material->set("uMaterialInfo.ao", 1.0f);
+
+                PS_ASSERT(sphere_material->bake(), "Failed to bake sphere material {} {}", metallic_idx, roughness_idx)
+
+                auto sphere_entity = m_scene->create_entity();
+                sphere_entity.get_component<Phos::TransformComponent>().position =
+                    glm::vec3(metallic_idx * 2, roughness_idx * 2 + 1, -2.0f);
+                sphere_entity.get_component<Phos::TransformComponent>().scale = glm::vec3(0.6f);
+                sphere_entity.get_component<Phos::TransformComponent>().rotation =
+                    glm::vec3(glm::radians(0.0f), 0.0f, 0.0f);
+                sphere_entity.add_component<Phos::MeshRendererComponent>({
+                    .mesh = sphere_mesh,
+                    .material = sphere_material,
+                });
+            }
+        }
+
+        // Lights
+        const std::vector<glm::vec3> light_positions = {
+            glm::vec3(2.0f, 3.0f, 1.5f),
+            glm::vec3(4.0f, 2.0f, 1.5f),
+            glm::vec3(5.0f, 6.0f, 1.5f),
+            glm::vec3(5.0f, 1.0f, 1.5f),
+        };
+
+        for (const auto& light_pos : light_positions) {
+            auto light_entity = m_scene->create_entity();
+            light_entity.get_component<Phos::TransformComponent>().position = light_pos;
+            light_entity.get_component<Phos::TransformComponent>().scale = glm::vec3(0.15f);
+
+            light_entity.add_component<Phos::LightComponent>({
+                .light_type = Phos::Light::Type::Point,
+                .color = glm::vec4(1.0f),
+            });
+
+            light_entity.add_component<Phos::MeshRendererComponent>({
+                .mesh = cube_mesh,
+                .material = light_material,
+            });
+        }
+
+        auto directional_light_entity = m_scene->create_entity();
+        directional_light_entity.get_component<Phos::TransformComponent>().position = glm::vec3(2.0f, 17.0f, 9.0f);
+        directional_light_entity.get_component<Phos::TransformComponent>().rotation =
+            glm::vec3(glm::radians(40.0f), glm::radians(180.0f), glm::radians(0.0f));
+        directional_light_entity.get_component<Phos::TransformComponent>().scale = glm::vec3(0.15f);
+
+        directional_light_entity.add_component<Phos::MeshRendererComponent>({
+            .mesh = cube_mesh,
+            .material = light_material,
+        });
+
+        directional_light_entity.add_component<Phos::LightComponent>({
+            .light_type = Phos::Light::Type::Directional,
+            .color = glm::vec4(1.0f),
+
+            .shadow_type = Phos::Light::ShadowType::Hard,
+        });
     }
 };
 
