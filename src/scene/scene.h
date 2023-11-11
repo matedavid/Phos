f227acb9e7c30dea3b265d1a866096228d814a82
matedavid <david.mate.lopez@gmail.com>
2023-11-11 01:24:14 +0100

    Finally compiling in clang

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -1,61 +1,21 @@
 #pragma once
 
-#include "core.h"
-
-#include "asset/asset.h"
-
-#include "scene/registry.h"
-#include "scene/components.h"
-#include "scene/scene_renderer.h"
+#include "scene/scene_definition.h"
+#include "scene/entity.h"
 
 namespace Phos {
 
-// Forward declarations
-class Camera;
-class Entity;
+template <typename... Components>
+[[nodiscard]] std::vector<Entity> Scene::get_entities_with() {
+    std::vector<std::size_t> ids = m_registry->view<Components...>();
 
-class Scene : public IAsset {
-  public:
-    explicit Scene(std::string name);
-    Scene(Scene& other);
-    virtual ~Scene();
+    std::vector<Entity> entities;
+    entities.reserve(ids.size());
 
-    [[nodiscard]] AssetType asset_type() override { return AssetType::Scene; }
+    for (const auto& entity_id : ids)
+        entities.push_back(*m_id_to_entity[entity_id]);
 
-    Entity create_entity();
-    Entity create_entity(UUID uuid);
-    Entity create_entity(const std::string& name, UUID uuid);
-
-    void destroy_entity(Entity entity);
-
-    [[nodiscard]] Entity get_entity_with_uuid(const UUID& uuid);
-    [[nodiscard]] std::vector<Entity> get_all_entities() const;
-
-    template <typename... Components>
-    [[nodiscard]] std::vector<Entity> get_entities_with() {
-        std::vector<std::size_t> ids = m_registry->view<Components...>();
-
-        std::vector<Entity> entities;
-        entities.reserve(ids.size());
-
-        for (const auto& entity_id : ids)
-            entities.push_back(*m_id_to_entity[entity_id]);
-
-        return entities;
-    }
-
-    [[nodiscard]] std::string name() const { return m_name; }
-    [[nodiscard]] SceneRendererConfig& config() { return m_renderer_config; }
-
-  private:
-    std::string m_name;
-    std::unique_ptr<Registry> m_registry;
-    SceneRendererConfig m_renderer_config;
-
-    std::unordered_map<std::size_t, Entity*> m_id_to_entity;
-    std::unordered_map<UUID, Entity*> m_uuid_to_entity;
-
-    friend class Entity;
-};
+    return entities;
+}
 
 } // namespace Phos
